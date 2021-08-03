1a0c321127954627f88fccabf640ff99f2bedfcd
Paulius B <paulspremium@gmail.com>
2021-08-03 20:31:21 +0300

    added SetTimeOut version

--- Taxi.js
+++ Taxi.js
@@ -1,24 +1,51 @@
 class Taxi {
     constructor() {
     }
-    drive(distance) {
-
+    print(km) {
         return new Promise((res, rej) => {
-            let currentDistance = 0;
-
-            //paleidziam cikla kuris kas sekunde ispausdina  nauja atstuma:
-
-            const timer = setInterval(() => {
-                console.log(`Nuvaziuotas atstumas ${++currentDistance} KM`);
-
-                if (currentDistance === distance) {
-                    clearTimeout(timer) //nukilinam cikla
-                    console.log(`Kelione baigta`);
-                    res(); //baigiam pazada
+            setTimeout(() => {
+                if (typeof km === 'number') {
+                    res(`Nuvaziuota: ${km} km.`);
+                } else {
+                    rej('Kilometrai turi buti skaiciai.');
                 }
             }, 1000);
         })
     }
+
+    async drive(distance) {
+        for (let i = 1; i <= distance; i++) {
+            await this.print(i)
+                .then((msg) => {
+                    console.log(msg);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+        console.log('Kelione baigta.');
+    }
+
+
+
+    // drive(distance) {
+
+    //     return new Promise((res, rej) => {
+    //         let currentDistance = 0;
+
+    //         //paleidziam cikla kuris kas sekunde ispausdina  nauja atstuma:
+
+    //         const timer = setInterval(() => {
+    //             console.log(`Nuvaziuotas atstumas ${++currentDistance} KM`);
+
+    //             if (currentDistance === distance) {
+    //                 clearTimeout(timer) //nukilinam cikla
+    //                 console.log(`Kelione baigta`);
+    //                 res(); //baigiam pazada
+    //             }
+    //         }, 1000);
+    //     })
+    // }
 }
 
 module.exports = Taxi;
